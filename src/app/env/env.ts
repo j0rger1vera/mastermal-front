9f60a082d9c3e47fb27d5f806ad7deb839122376
j0rger1vera <96147065+j0rger1vera@users.noreply.github.com>
2025-06-26 08:47:49 -0500

    Update env.ts

--- src/app/env/env.ts
+++ src/app/env/env.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  //ApiUrl: 'http://localhost:8080'
+  ApiUrl: 'http://localhost:8080'
   //ApiUrl: 'http://factura-env.eba-e3t3gae3.us-east-1.elasticbeanstalk.com'
-  ApiUrl: 'https://mastermalbackend.onrender.com'
+  //ApiUrl: 'https://mastermalbackend.onrender.com'
 };
