50ba56572a73195cfc102b459122975989092d11
j0rger1vera <96147065+j0rger1vera@users.noreply.github.com>
2025-06-27 03:09:53 -0500

    Update facturacion.component.ts

--- src/app/components/facturas/facturacion/facturacion.component.ts
+++ src/app/components/facturas/facturacion/facturacion.component.ts
@@ -11,6 +11,8 @@ export class ListaFacturacionComponent {
   facturas: any ; 
   facturasBalance: any ;
   facturasFiltradas: any;
+  facturasPorNit: any;
+  abonoValue: any;
   modoOculto: boolean = true;
   constructor(private facturasService: FacturasService) {
   }
@@ -50,12 +52,67 @@ export class ListaFacturacionComponent {
     const input = texto.target as HTMLInputElement;
     const inputLowerCase = input.value.toLowerCase();
     console.log(this.facturasFiltradas)
+    
     this.facturasFiltradas = this.facturas.filter( (factura: any) =>
       factura.nitCliente.toString().includes(input.value) ||
       factura.nombreCliente.toString().includes(inputLowerCase)
     );
   }
 
+  onEnter(factura: any, abonoInput: HTMLInputElement) {
+    const abono = abonoInput.value;
+    if (abono) {
+      this.abonoValue = parseFloat(abono);
+      if (!isNaN(this.abonoValue) && this.abonoValue > 0) {
+        factura.abono = factura.abono + this.abonoValue;
+        factura.saldo = factura.saldo - this.abonoValue;
+        console.log('Abono actualizado:', factura);
+        
+        abonoInput.value = '';
 
+        this.facturasService.getFacturacionPorClienteId(factura.rucCliente).subscribe(data => {
+          this.facturasPorNit = data;
+
+            // Recorrer la lista de facturas y actualizar cada una
+                this.facturasPorNit.forEach((facturaPorNit: any) => {
+                    // Solo actualizar si abonoValue es mayor que cero
+                    if (this.abonoValue > 0) {//75000
+                        // Actualizar la factura
+                            
+                            if (this.abonoValue > facturaPorNit.saldo) { 
+                              this.abonoValue = this.abonoValue - facturaPorNit.saldo; // 67500                            
+                              facturaPorNit.abono = facturaPorNit.saldo;
+                            } else {
+                              facturaPorNit.abono = this.abonoValue;
+                            }
+                            facturaPorNit.saldo = facturaPorNit.abono - facturaPorNit.saldo;
+
+                            // Si el abonoValue llega a cero, se puede salir del ciclo
+                            if (this.abonoValue <= 0) {
+                                console.log('Todos los abonos procesados.');
+                            }
+
+                        this.facturasService.actualizar(facturaPorNit).subscribe(response => {
+                            console.log('Datos enviados correctamente:', response);
+                        }, error => {
+                            console.error('Error al enviar datos:', error);
+                            alert('Error al enviar datos: los campos no cumplen con los formatos requeridos');
+                        });
+                    }
+                });
+
+                // Al final, si se quiere mostrar un mensaje de éxito
+                if (this.abonoValue <= 0) {
+                    alert('Todos los datos registrados correctamente');
+                }
+          });
+
+      } else {
+        console.error('El valor ingresado no es un número válido');
+      }
+    } else {
+      console.error('No se ingresó ningún valor');
+    }
+  }
 
 }
